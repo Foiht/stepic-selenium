93c9871f713962c36f14318042347ea26e7cb051
Foiht <46824092+Foiht@users.noreply.github.com>
2019-07-31 19:33:32 +0300

    Delete S2_4.py

--- S2_4.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-br = webdriver.Chrome()
-br.get('http://suninjuly.github.io/wait1.html')
-br.implicitly_wait(5)
-br.find_element_by_id('check').click()
-test = br.find_element_by_id('check_message').text
-assert 'успешно' in test
