3dc511e2d4d6cdf68f240ffde218e4d38507de31
Foiht <46824092+Foiht@users.noreply.github.com>
2019-07-31 19:37:35 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-def test_language(browser):
+def test_guest_should_see_basket_link_in_his_language(browser):
     link = ' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     browser.find_element_by_css_selector('.btn-add-to-basket')
